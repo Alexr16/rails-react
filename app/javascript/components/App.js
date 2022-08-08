4021e51ea52a9faa1bc470fd5a46d0c7f06089e0
Alexr16 <j.alex-1996@hotmail.com>
2022-08-08 17:02:55 -0500

    Refactor app

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,16 +1,22 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Greeting from './Greeting';
-// import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
 
-const App = () => {
-  // <Provider store={store}>
-  <Router>
-    <Routes>
-      <Route exact path="" element={<Greeting />} />
-    </Routes>
-  </Router>;
-  // </Provider>
-};
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Greeting />} />
+          </Routes>
+        </Router>
+      </Provider>
+    );
+  }
+}
 
 export default App;
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchmessage } from '../redux/Messages/Messages';
+import { useSelector } from 'react-redux';
 
 const Greeting = () => {
   const message = useSelector((state) => state.messageReducer.message);
-  console.log(message);
 
   return (
     <div>
